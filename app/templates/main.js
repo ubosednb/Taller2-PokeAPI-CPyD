1c755466b45011c3ca839d2059c4126c2b56b087
matalvarez10 <matias.alvarezc@utem.cl>
2022-10-25 23:48:33 -0300

    adding colours

--- app/templates/main.js
+++ app/templates/main.js
@@ -38,6 +38,28 @@ function fetchContador(offset, limit){
     }
 }
 
+const colors = {
+	normal: '#A8A77A',
+	fire: '#EE8130',
+	water: '#6390F0',
+	electric: '#F7D02C',
+	grass: '#7AC74C',
+	ice: '#96D9D6',
+	fighting: '#C22E28',
+	poison: '#A33EA1',
+	ground: '#E2BF65',
+	flying: '#A98FF3',
+	psychic: '#F95587',
+	bug: '#A6B91A',
+	rock: '#B6A136',
+	ghost: '#735797',
+	dragon: '#6F35FC',
+	dark: '#705746',
+	steel: '#B7B7CE',
+	fairy: '#D685AD',
+};
+const main_types = Object.keys(colors);
+
 function createPokemon(pokemon){
 
     const card = document.createElement('div');
@@ -78,9 +100,15 @@ function createPokemon(pokemon){
     let cant_tipos = pokemon.types.length;
     for(let i = 0; i < cant_tipos; i++){
         const tipo = document.createElement('p');
+        type = pokemon.types[i].type.name.toString();
         tipo.classList.add('tipo');
-        tipo.textContent = `Tipo: ${pokemon.types[i].type.name.toString()}`;
+        tipo.textContent = `Tipo: ${type}`;
         card.appendChild(tipo);
+        if(i==0){
+            //AGREGANDO COLOR
+            const color = colors[type];
+            card.style.backgroundColor = color;
+        }
     }
     
     //FORMA
