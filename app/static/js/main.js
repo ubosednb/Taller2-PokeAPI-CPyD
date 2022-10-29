396d1d140eebbc867939f9a779ef0d09b60e1fab
matalvarez10 <matias.alvarezc@utem.cl>
2022-10-29 03:20:14 -0300

    agregando comentarios al codigo

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -6,6 +6,7 @@ const siguiente = document.querySelector("#siguiente");
 let offset = 1;
 let limit = 9;
 
+//Funcion para la paginacion y boton anterior
 anterior.addEventListener('click', () => {
     if(offset != 1){
         offset = offset-10;
@@ -13,13 +14,14 @@ anterior.addEventListener('click', () => {
         fetchContador(offset, limit);
     }
 })
-
+//Funcion para la paginacion y boton siguiente
 siguiente.addEventListener('click', () => {
     offset = offset+10;
     removeChildNodes(container);
     fetchContador(offset, limit);
 })
 
+// Funcion que recupera todos los pokemon desde la ruta de la API, a traves de su ID
 async function fetchPokemon(id){
     await fetch(`http://127.0.0.1:5000/pokemon/${id}`)
     .then((res) => res.json())
@@ -28,14 +30,14 @@ async function fetchPokemon(id){
         spinner.style.display = "none";
     });
 }
-
+//Funcion de control para la paginacion
 async function fetchContador(offset, limit){
     spinner.style.display = "block";
     for(let i = offset; i <= offset + limit; i++){
         await fetchPokemon(i);
     }
 }
-
+//Agregando colores
 const colors = {
 	normal: '#A8A77A',
 	fire: '#EE8130',
@@ -57,6 +59,7 @@ const colors = {
 	fairy: '#D685AD',
 };
 
+// Agregando traducciones
 const translations = {
 	normal: 'Normal',
 	fire: 'Fuego',
@@ -79,13 +82,15 @@ const translations = {
 };
 const main_types = Object.keys(colors);
 
+// Funcion que crea cada unos de los pokemon, para luego ser agregados al html
 function createPokemon(pokemon){
 
+    //Se crea el div que contenera todos los datos del pokemon
     const card = document.createElement('div');
     card.classList.add('pokemon-block');
 
     //SPRITE
-    const spriteContainer = document.createElement('div');
+    const spriteContainer = document.createElement('div');       // Se rescata el sprite desde el objeto pokemon
     const sprite = document.createElement('img');
     sprite.src = pokemon.sprites.front_default;
     sprite.classList.add('spriteimg');
@@ -93,125 +98,125 @@ function createPokemon(pokemon){
     card.appendChild(spriteContainer);
 
     //ID
-    const num = document.createElement('p');
+    const num = document.createElement('p');             // Se le agrega el ID en un p tag
     num.classList.add('name');
     num.textContent = `#${pokemon.id.toString().padStart(3, 0)}`;
     card.appendChild(num);
 
     //NOMBRE
-    const name = document.createElement('p');
+    const name = document.createElement('p');          // Se le agrega el nombre en un p tag
     name.classList.add('name');
     name.textContent = pokemon.name;
     card.appendChild(name);
 
     //PESO Y ALTURA
-    var contenedor = document.createElement('div');
+    var contenedor = document.createElement('div');  // Se crea contenedor para ingresar el peso y altura
     contenedor.classList.add('contenenedor');
-    var contenedorpeso = document.createElement('div');
+    var contenedorpeso = document.createElement('div');  // Se crea contenedor para el peso
     contenedorpeso.classList.add('subcont');
-    var contenedoraltura = document.createElement('div');
+    var contenedoraltura = document.createElement('div');  // Se crea contenedor para la altura
     contenedoraltura.classList.add('subcont');
 
     var imgpeso = document.createElement("img");
     imgpeso.classList.add('imgstyle');
-    imgpeso.setAttribute("src", "./static/img/Vector.png");
+    imgpeso.setAttribute("src", "./static/img/Vector.png");   // Agregando Icono para el peso
     contenedorpeso.appendChild(imgpeso);
-    var imgaltura = document.createElement("img");
+    var imgaltura = document.createElement("img");               
     imgaltura.classList.add('imgstyle');
-    imgaltura.setAttribute("src", "./static/img/height.png");
+    imgaltura.setAttribute("src", "./static/img/height.png");  // Agregando icono para la altura
     contenedoraltura.appendChild(imgaltura);
 
-    var textpeso = document.createElement('div');
+    var textpeso = document.createElement('div');                 // Agregando texto que contiene el peso
     textpeso.textContent=`${pokemon.weight.toString()} kg`;
     textpeso.classList.add('parrafo');
-    var textweight = document.createElement('div');
+    var textweight = document.createElement('div');               
     textweight.textContent="Peso";
     textweight.classList.add('parrafo2');
 
-    var textaltura = document.createElement('div');
-    textaltura.textContent=`${pokemon.height.toString()} m`;
-    textaltura.classList.add('parrafo');
-    var textheight = document.createElement('div');
-    textheight.textContent="Altura";
+    var textaltura = document.createElement('div');                  //Agregando texto que contiene la altura
+    textaltura.textContent=`${pokemon.height.toString()} m`;          // Se rescasta el peso de la altura
+    textaltura.classList.add('parrafo');                            
+    var textheight = document.createElement('div'); 
+    textheight.textContent="Altura";                               // Se agrega texto altura
     textheight.classList.add('parrafo2');
 
-    contenedorpeso.appendChild(textpeso);
-    contenedorpeso.appendChild(textweight);
-    contenedor.appendChild(contenedorpeso);
-    contenedoraltura.appendChild(textaltura);
-    contenedoraltura.appendChild(textheight);
+    contenedorpeso.appendChild(textpeso);                          // Se añade el texto al contenedor
+    contenedorpeso.appendChild(textweight);                        // Se añade el texto al contenedor
+    contenedor.appendChild(contenedorpeso);                        // Se añade el subcontenedor de peso al contenedor principal
+    contenedoraltura.appendChild(textaltura);                      // Se añade el texto al contenedor
+    contenedoraltura.appendChild(textheight);                      // Se añade el texto al contenedor
     contenedor.appendChild(contenedoraltura);
-    card.appendChild(contenedor);
+    card.appendChild(contenedor);                             // Se añade toda la seccion de peso y altura a la carta
 
     //TIPO
-    const containerTipos= document.createElement('div')
-    containerTipos.classList.add('typescontainer');
-    let cant_tipos = pokemon.types.length;
-    const textotipo = document.createElement('div');
+    const containerTipos= document.createElement('div')          // Se crean contenedores para el tipo
+    containerTipos.classList.add('typescontainer');              // Se le añade la clase desde css
+    let cant_tipos = pokemon.types.length;                      // Cantidad de tipos de cada pokemon ( 1 o 2)
+    const textotipo = document.createElement('div');           // texto para el tipo
     textotipo.classList.add('textotipo');
     textotipo.textContent = "Tipo(s):";
     card.appendChild(textotipo);
 
-    for(let i = 0; i < cant_tipos; i++){
-        const type = pokemon.types[i].type.name.toString();
+    for(let i = 0; i < cant_tipos; i++){                             // Se recorre por cada tipo que posee cada pokemon
+        const type = pokemon.types[i].type.name.toString();         //  Se rescata el string correspondiente al tipo
         const pTipo=document.createElement('p')
         pTipo.classList.add('pTipo');
-        const tipoEsp = translations[type];
-        pTipo.textContent = `${tipoEsp}`;
+        const tipoEsp = translations[type];                            // Se traduce al Español
+        pTipo.textContent = `${tipoEsp}`;                             // Se añade al div del tipo
         const tipo = document.createElement('div');
-        const color = colors[type];
-        tipo.classList.add('types');
-        tipo.appendChild(pTipo)
-        tipo.style.backgroundColor = color;
+        const color = colors[type];                              // Se añade su color correspondiente
+        tipo.classList.add('types');                            
+        tipo.appendChild(pTipo)                       
+        tipo.style.backgroundColor = color;              // Se le añade el color por el style
         containerTipos.appendChild(tipo)
         card.appendChild(containerTipos);
         if(i==0){
             //AGREGANDO COLOR
-            const color = colors[type];
+            const color = colors[type];                       // Se le añade color al fondo de la imagen del pokemon
             spriteContainer.style.backgroundColor = color;
-            spriteContainer.style.borderRadius = '10px';
+            spriteContainer.style.borderRadius = '10px';         // Se le añade borde circular al espacio correspondiente a la imagen
         }
     }
     
     //FORMA
-    let cant_forma = pokemon.forms.length;
-    for(let i = 0; i < cant_forma; i++){
+    let cant_forma = pokemon.forms.length;                             // Se rescara la forma desde el objeto pokemon
+    for(let i = 0; i < cant_forma; i++){                             // Se recorre cada forma que tenga el pokemon
         const forma = document.createElement('p');
-        if(pokemon.forms[i].name.toString() == pokemon.name){
-            forma.textContent = "Forma: Original";
+        if(pokemon.forms[i].name.toString() == pokemon.name){             // Si es que la forma es igual al nombre, significa que es la original
+            forma.textContent = "Forma: Original";                        
         }
         else{
-            forma.textContent = `Forma: ${pokemon.forms[i].name.toString()}`; 
+            forma.textContent = `Forma: ${pokemon.forms[i].name.toString()}`;  // Si no es igual se muestra su forma en la carta
         }
         card.appendChild(forma);
     }
 
     //HABILIDAD
-    let cant_hab = pokemon.abilities.length;
-    const textohab = document.createElement('div');
+    let cant_hab = pokemon.abilities.length;                   // Se rescata la habilidad desde el objeto pokemon
+    const textohab = document.createElement('div');            
     card.appendChild(textohab);
     for(let i = 0; i < cant_hab; i++){
-        const habi = document.createElement('p');
+        const habi = document.createElement('p');               // Se crea un p tag por elemento
         var aux = pokemon.abilities;
         
-        habi.classList.add('habi');
-        if(aux[i].is_hidden == false){
+        habi.classList.add('habi');                                                 
+        if(aux[i].is_hidden == false){                     // Se revisa si es una habilidad oculta
             var aux2 = pokemon.abilities[i].ability.name.toString();
             aux2 = aux2.replace("-"," ");
-            aux2 = aux2.charAt(0).toUpperCase() + aux2.slice(1);
+            aux2 = aux2.charAt(0).toUpperCase() + aux2.slice(1); // Dandole formato al string
             habi.textContent = `Habilidad: ${aux2}\t`;
         }
         else{
             var aux2 = pokemon.abilities[i].ability.name.toString();
             aux2 = aux2.replace("-"," ");
-            aux2 =  aux2.charAt(0).toUpperCase() + aux2.slice(1);
+            aux2 =  aux2.charAt(0).toUpperCase() + aux2.slice(1);  // Dandole formato al string
             habi.textContent = `Habilidad Oculta: ${aux2}\t`;
         }
         card.appendChild(habi);
     }
 
     //UBICACION
-    const location = document.createElement('p');
+    const location = document.createElement('p');         // Se le asigna su ubicacion en base a su region, y su id  
     location.classList.add('name');
     if(pokemon.id>=1 && pokemon.id<=151){
         location.textContent = `Ubicación: Kanto`;
@@ -248,4 +253,4 @@ function removeChildNodes(parent){
     }
 }
 
-fetchContador(offset, limit);
+fetchContador(offset, limit);  // Inicializando funcion
