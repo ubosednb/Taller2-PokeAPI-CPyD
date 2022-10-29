88d2e7265f2de0a27ab296089a93794dca6f4fb0
ubosednb <cabarcaa@utem.cl>
2022-10-28 22:25:24 -0300

    consumo de 2da api

--- app/app.py
+++ app/app.py
@@ -4,9 +4,15 @@ from flask import render_template
 import requests
 
 URL_pokemon = 'https://pokeapi.co/api/v2/pokemon/'
+URL_pokemons = 'https://pokeapi.co/api/v2/pokemon?offset='
 
 app = Flask(__name__)
 CORS(app)
+
+def pokes_api(inicio, final):
+    get_pokemons = requests.get(f"{URL_pokemons}{inicio}{'&limit='}{final}")
+    names = get_pokemons.json()
+    return names
 
 def poke_api(pok):
     get_pokemon = requests.get(f"{URL_pokemon}{pok}")
@@ -17,6 +23,11 @@ def poke_api(pok):
 def find_esentials(id):
     pokemon_info = poke_api(id)
     return pokemon_info
+
+@app.route("/pokemons/<id_1>/<id_2>")
+def find_pokemons(id_1, id_2):
+    pokemons_info = pokes_api(id_1, id_2)
+    return pokemons_info
 
 @app.route("/")
 def index():
