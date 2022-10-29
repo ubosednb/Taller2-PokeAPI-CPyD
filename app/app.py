cfae19e45d83af7a250860ab97bd06cdc0c28010
Meshdako <msobarzo@utem.cl>
2022-10-29 03:10:45 -0300

    Ruta de ABOUT agregada

--- app/app.py
+++ app/app.py
@@ -33,6 +33,10 @@ def find_pokemons(id_1, id_2):
 def pokemons():
     return render_template('./pokes.html')
 
+@app.route("/about")
+def about():
+    return render_template('./about.html')
+
 @app.route("/")
 def index():
     return render_template('./index.html')
